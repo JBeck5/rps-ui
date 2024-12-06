3d5307e4d0d885de5be0e84373a5dce85bcc3805
JBeck5 <73002731+JBeck5@users.noreply.github.com>
2024-12-06 12:29:11 -0800

    adding ui and text/img change

--- script.js
+++ script.js
@@ -1,66 +1,89 @@
+const cSelection = document.getElementById("computer"),
+rpsButton = document.querySelectorAll(".playerBtn"),
+resultMsg = document.querySelector("#results"),
+resultDescription = document.querySelector("#results-description"),
+playerScoreboard = document.querySelector("#player-score"),
+computerScoreboard = document.querySelector("#computer-score");
+
+
+rpsButton.forEach((btn) => btn.addEventListener('click', playGame));
+let playerScore = 0,
+computerScore = 0;
+
+function playGame() {
+    let playerChoice = this.id;
+    let computerChoice = getComputerChoice();
+    let results = playRound(playerChoice, computerChoice);
+    updateScore(playerChoice, computerChoice, results);
+}
+
 function getComputerChoice () {
     let options = ["rock", "paper", "scissors"];
     pick = Math.floor(Math.random() * options.length)
     return options[pick];
 }
 
-function getHumanChoice () {
-    let pick = prompt("Input: Rock, Paper, Scissors").toLowerCase();
-    if (pick != "rock" && pick != "paper" && pick != "scissors") {
-        return getHumanChoice();
-    } else return pick;
-}
-
 function playRound(humanChoice, computerChoice) {
-    console.log (`You: ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}  |  
-                CPU: ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`);
-
+    let result; // Tie:0, playerWin:1, playerLose:2
     switch(humanChoice) {
         case "rock":
             if (computerChoice === "rock") {
-                console.log("Tie");
+                return result = 0;
                 } else if (computerChoice === "scissors") {
-                console.log("You Win");
-                humanScore++;
+                    return result = 1;
             } else if (computerChoice === "paper") {
-                console.log("You Lose");
-                computerScore ++;
-            } break;
+                return result = 2;
+            }
         case "paper":
             if (computerChoice === "paper") {
-                console.log("Tie");
+                return result = 0;
             } else if (computerChoice === "rock") {
-                console.log("You Win");
-                humanScore++;
+                return result = 1;
             } else if (computerChoice === "scissors") {
-                console.log("You Lose");
-                computerScore ++;
-            } break;
+                return result = 2;
+            }
         case "scissors":
             if (computerChoice === "scissors") {
-                console.log("Tie");
+                return result = 0;
             } else if (computerChoice === "paper") {
-                console.log("You Win");
-                humanScore++;
+                return result = 1;
             } else if (computerChoice === "rock") {
-                console.log("You Lose");
-                computerScore ++;
-            } break;
+                return result = 2;
+            }
     }
 
 }
 
-function playGame() {
-    for (let i = 1; i <= 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+function updateScore(pChoice, cChoice, outcome) {
+/*  update computer img
+    update player & computer score
+    display outcome of round
+    display outcome winner
+    check for game winner (5 wins)
+        if true, display game over & play again button
+            if play again, clean reset
+*/
+    if (outcome == 0) {
+        resultMsg.textContent = 'It\'s a Tie.';
+    }else if (outcome == 1) {
+        resultMsg.textContent = 'You Win.';
+        playerScoreboard.textContent = `${playerScore + 1}`;
+        playerScore++;
+    } else if (outcome == 2) {
+        resultMsg.textContent = 'You Lose.';
+        computerScoreboard.textContent = `${computerScore + 1}`;
+        computerScore++;
+    }
+    resultDescription.textContent = `You chose ${pChoice} and Computer chose ${cChoice}.`;
+    switch(cChoice) {
+        case 'rock':
+            cSelection.src = "./images/rock.png";
+            break;
+        case 'paper':
+            cSelection.src = "./images/paper.png";
+            break;
+        case 'scissors':
+            cSelection.src = "./images/scissors.png";
+            break;
     }
 }
-
-let computerScore = 0;
-let humanScore = 0;
-playGame();
-
-console.log("     Final Score:");
-console.log(`You: ${humanScore}  |  CPU: ${computerScore}`);
